615aa612625539aed02419034eeda09f8f59a73f
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2022-03-14 01:55:35 -0600

    Agregando endpoint searchComics con metodo GET consumiendo el endpoint

--- src/app.py
+++ src/app.py
@@ -4,8 +4,11 @@ import requests
 
 from dotenv import load_dotenv
 from pprint import pprint
+from flask import Flask, jsonify, request, Response
 
 load_dotenv()
+
+app = Flask(__name__)
 
 TS=1
 PUBLIC_KEY = os.getenv('PUBLIC_KEY')
@@ -13,26 +16,30 @@ PRIVATE_KEY = os.getenv('PRIVATE_KEY')
 HASHED = os.getenv('HASHED')
 URL_CHARACTERS = f"https://gateway.marvel.com:443/v1/public/characters?ts={TS}&apikey={PUBLIC_KEY}&hash={HASHED}"
 
-response_characters = requests.get(URL_CHARACTERS)
+@app.route('/searchComics/<search>', methods=["GET"])
+def get_character(search):
+  response_characters = requests.get(URL_CHARACTERS)
 
-if response_characters.status_code==200:
-  response_json = json.loads(response_characters.text)
+  if response_characters.status_code==200:
+    response_json = json.loads(response_characters.text)
 
-  for character in response_json["data"]["results"]:
-    
-    character_to_search = "3-D Man"
-    
-    if character["name"] in character_to_search:
-      id = character["id"]
-      name = character["name"]
-      image = character["thumbnail"]["path"]+"."+character["thumbnail"]["extension"]
-      appearances = character["comics"]["available"]
+    for character in response_json["data"]["results"]:
+      
+      if character["name"] == search:
+        id = character["id"]
+        name = character["name"]
+        image = character["thumbnail"]["path"]+"."+character["thumbnail"]["extension"]
+        appearances = character["comics"]["available"]
 
-      dic = {
-        "id":id,
-        "name":name,
-        "image":image,
-        "appearances":appearances
-      }
+        response = {
+          "id":id,
+          "name":name,
+          "image":image,
+          "appearances":appearances
+        }
+        return jsonify(response)
+    response = jsonify({'message': 'No results for ' + search})
+    return response
 
-      pprint(dic)
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=4000, debug=True)
