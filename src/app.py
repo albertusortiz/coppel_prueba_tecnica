26371b9db05627a371657243337c25e121032e46
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2022-03-14 01:07:36 -0600

    Printing the result of a character searched.

--- src/app.py
+++ src/app.py
@@ -11,10 +11,28 @@ TS=1
 PUBLIC_KEY = os.getenv('PUBLIC_KEY')
 PRIVATE_KEY = os.getenv('PRIVATE_KEY')
 HASHED = os.getenv('HASHED')
-URL = f"https://gateway.marvel.com:443/v1/public/characters?ts={TS}&apikey={PUBLIC_KEY}&hash={HASHED}"
+URL_CHARACTERS = f"https://gateway.marvel.com:443/v1/public/characters?ts={TS}&apikey={PUBLIC_KEY}&hash={HASHED}"
 
-response = requests.get(URL)
+response_characters = requests.get(URL_CHARACTERS)
 
-if response.status_code==200:
-  response_json = json.loads(response.text)
-  pprint(response_json["data"]["results"][0])
+if response_characters.status_code==200:
+  response_json = json.loads(response_characters.text)
+
+  for character in response_json["data"]["results"]:
+    
+    character_to_search = "3-D Man"
+    
+    if character["name"] in character_to_search:
+      id = character["id"]
+      name = character["name"]
+      image = character["thumbnail"]["path"]+"."+character["thumbnail"]["extension"]
+      appearances = character["comics"]["available"]
+
+      dic = {
+        "id":id,
+        "name":name,
+        "image":image,
+        "appearances":appearances
+      }
+
+      pprint(dic)
